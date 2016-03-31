fe61d5feb4f74880acf6412ccabd04ddc1f06bd5
SiKang <940142543@qq.com>
2016-03-31 17:48:49 +0800

    target 2

--- app/src/main/java/sikang/drawtest/DrawView.java
+++ app/src/main/java/sikang/drawtest/DrawView.java
@@ -1,5 +1,6 @@
 package sikang.drawtest;
 
+import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Context;
@@ -20,7 +21,7 @@ import java.util.List;
 /**
  * Created by SiKang on 2016/3/28.
  */
-public class DrawView extends View {
+public class DrawView extends View implements Animator.AnimatorListener {
     private final String TAG = "DrawTest";
     private Canvas mCanvas;
     private Bitmap mDrawBitmap;
@@ -29,7 +30,7 @@ public class DrawView extends View {
     private Path mPath;
     private Paint mPaint;
     private float mLeft, mTop, mRight, mBottom;
-    private boolean rectUpdated;
+    private boolean rectUpdated, isEnlarging;
 
     public DrawView(Context context) {
         super(context);
@@ -43,6 +44,8 @@ public class DrawView extends View {
 
     private void init() {
         rectUpdated = false;
+        isEnlarged = false;
+        isEnlarging = false;
         linePoints = new ArrayList<>();
         pathList = new ArrayList<Path>();
         mPath = new Path();
@@ -53,7 +56,7 @@ public class DrawView extends View {
         mPaint.setStrokeWidth(20);
         mPaint.setAlpha(110);
         mPaint.setStrokeJoin(Paint.Join.ROUND);
-        mPaint.setStrokeCap(Paint.Cap.ROUND);// 圆心笔触
+        mPaint.setStrokeCap(Paint.Cap.ROUND);// 圆形笔触
 
     }
 
@@ -109,44 +112,106 @@ public class DrawView extends View {
 
     }
 
-    private float lastTouchX, lastTouchY;
+    private float lastTouchX, lastTouchY, startRawX, startRawY;
+    private boolean isDoubleTouch, isDoubleClick, isEnlarged;
+    private long lastClickTime;
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        int action = event.getAction();
         float eventX = event.getX();
         float eventY = event.getY();
         //阻止父级拦截
-        switch (action) {
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
             case MotionEvent.ACTION_DOWN:
-                getParent().requestDisallowInterceptTouchEvent(true);
-                lastTouchX = eventX;
-                lastTouchY = eventY;
-                mPath.moveTo(eventX, eventY);
-                updateRect(eventX, eventY);
-                linePoints.add(new LinePoint(eventX, eventY, true));
+                long time = System.currentTimeMillis();
+                isDoubleClick = false;
+                if (!isEnlarging && time - lastClickTime <= 1000) {
+                    if (Math.abs(event.getRawX() - startRawX) <= 20 && Math.abs(event.getRawY() - startRawY) <= 20) {
+                        if (isEnlarged) {
+                            scaleViewAnimator(3f, -(eventX - getWidth() / 2), -(eventY - getHeight() / 2));
+                        } else {
+                            scaleViewAnimator(1f, 0f, 0f);
+                        }
+                        isDoubleClick = true;
+                        isEnlarged = !isEnlarged;
+                        lastClickTime = 0;
+                    }
+                }
+                startRawX = event.getRawX();
+                startRawY = event.getRawY();
+                lastClickTime = time;
+                touchDown(eventX, eventY);
                 break;
             case MotionEvent.ACTION_MOVE:
-                touchMove(event);
-                updateRect(eventX, eventY);
-                linePoints.add(new LinePoint(eventX, eventY, false));
+                if (!isDoubleClick) {
+                    if (event.getPointerCount() == 1)
+                        if (!isDoubleTouch)
+                            touchMove(event);
+                    if (event.getPointerCount() == 2) {
+                        isDoubleTouch = true;
+                        if (!isEnlarged)
+                            moveView(event.getRawX(), event.getRawY());
+                    }
+                } else {
+                    scaleView(event.getRawX(), event.getRawY());
+                }
                 break;
             case MotionEvent.ACTION_UP:
-                getParent().requestDisallowInterceptTouchEvent(true);
-                touchUp(eventX, eventY);
-                updateRect(eventX, eventY);
-                linePoints.add(new LinePoint(eventX, eventY, false));
+                if (isDoubleClick) {
+                } else if (!isDoubleTouch) {
+                    touchUp(eventX, eventY);
+                } else
+                    isDoubleTouch = false;
+
                 break;
         }
         invalidate();
         return true;
     }
 
+
+    private void touchDown(float x, float y) {
+        getParent().requestDisallowInterceptTouchEvent(true);
+        lastTouchX = x;
+        lastTouchY = y;
+        mPath.moveTo(x, y);
+        updateRect(x, y);
+        mScale = getScaleX();
+        linePoints.add(new LinePoint(x, y, true));
+
+    }
+
+    private void moveView(float rawX, float rawY) {
+        setTranslationX(getTranslationX() + (rawX - startRawX));
+        setTranslationY(getTranslationY() + (rawY - startRawY));
+        startRawX = rawX;
+        startRawY = rawY;
+    }
+
+    private float mScale;
+
+    private void scaleView(float rawX, float rawY) {
+        float scale;
+        if (Math.abs(rawX - startRawX) > Math.abs(rawY - startRawY)) {
+            scale = mScale + (rawX - startRawX) / getWidth();
+
+        } else {
+            scale = mScale + (rawY - startRawY) / getHeight();
+        }
+        setScaleX(scale);
+        setScaleY(scale);
+    }
+
     private void touchMove(MotionEvent event) {
         int historySize = event.getHistorySize();
         for (int i = 0; i < historySize; i++) {
-            addLine(event.getHistoricalX(i), event.getHistoricalY(i));
+            float historyX = event.getHistoricalX(i);
+            float historyY = event.getHistoricalY(i);
+            addLine(historyX, historyY);
+            updateRect(historyX, historyY);
+            linePoints.add(new LinePoint(historyX, historyY, false));
         }
+
     }
 
     private void addLine(float x, float y) {
@@ -180,9 +245,9 @@ public class DrawView extends View {
             return;
         rectUpdated = false;
         mPath.reset();
-        float viewWidth =(mRight - mLeft) + 20;
+        float viewWidth = (mRight - mLeft) + 20;
         float viewHeight = (mBottom - mTop) + 20;
-        initCanvas((int)viewWidth, (int)viewHeight);
+        initCanvas((int) viewWidth, (int) viewHeight);
         for (LinePoint point : linePoints) {
             float newX = point.x - mLeft + 10;
             float newY = point.y - mTop + 10;
@@ -199,14 +264,14 @@ public class DrawView extends View {
         mPath.reset();
         layout(getLeft() + (int) mLeft, getTop() + (int) mTop, getLeft() + (int) mRight + 20, getTop() + (int) mBottom + 20);
         invalidate();
-        float viewCenterX=(float)getLeft()+getWidth()/2;
-        float viewCenterY=(float)getTop()+getHeight()/2;
-        Log.d(TAG,"x: "+ 540/viewCenterX+"  y: "+400/viewCenterY);
-        scaleView(500 / viewWidth, 500 / viewHeight, 540 - viewCenterX, 400-viewCenterY);
+        float viewCenterX = (float) getLeft() + getWidth() / 2;
+        float viewCenterY = (float) getTop() + getHeight() / 2;
+        Log.d(TAG, "x: " + 540 / viewCenterX + "  y: " + 400 / viewCenterY);
+        scaleMoveView(500 / viewWidth, 500 / viewHeight, 540 - viewCenterX, 400 - viewCenterY);
     }
 
 
-    public void scaleView(float scaleX, float scaleY, float tranX, float tranY) {
+    public void scaleMoveView(float scaleX, float scaleY, float tranX, float tranY) {
         ObjectAnimator viewScaleX = ObjectAnimator.ofFloat(this, "scaleX", 1f, scaleX);
         ObjectAnimator viewScaleY = ObjectAnimator.ofFloat(this, "scaleY", 1f, scaleY);
         ObjectAnimator viewTranX = ObjectAnimator.ofFloat(this, "translationX", 0f, tranX);
@@ -215,10 +280,21 @@ public class DrawView extends View {
         animSet.play(viewScaleX).with(viewScaleY).with(viewTranX).with(viewTranY);
         animSet.setDuration(600);
         animSet.start();
+    }
 
+    public void scaleViewAnimator(float scale, float tranX, float tranY) {
+        AnimatorSet animSet = new AnimatorSet();
+        ObjectAnimator viewScaleX = ObjectAnimator.ofFloat(this, "scaleX", getScaleX(), scale);
+        ObjectAnimator viewScaleY = ObjectAnimator.ofFloat(this, "scaleY", getScaleX(), scale);
+        ObjectAnimator viewTranX = ObjectAnimator.ofFloat(this, "translationX", getTranslationX(), tranX);
+        ObjectAnimator viewTranY = ObjectAnimator.ofFloat(this, "translationY", getTranslationY(), tranY);
+        animSet.play(viewScaleX).with(viewScaleY).with(viewTranX).with(viewTranY);
+        animSet.setDuration(300);
+        animSet.start();
     }
 
     private void touchUp(float x, float y) {
+        getParent().requestDisallowInterceptTouchEvent(true);
         float cx1 = lastTouchX + (x - lastTouchX) / 4;
         float cy1 = lastTouchY + (y - lastTouchY) / 4;
         float cx2 = lastTouchX + (x - lastTouchX) / 4 * 3;
@@ -233,6 +309,8 @@ public class DrawView extends View {
 //        }
         mCanvas.drawPath(mPath, mPaint);
         mPath.reset();
+        updateRect(x, y);
+        linePoints.add(new LinePoint(x, y, false));
     }
 
     public void onDestory() {
@@ -246,6 +324,34 @@ public class DrawView extends View {
         if (pathList != null) {
             pathList = null;
         }
+
+    }
+
+    @Override
+    public void onAnimationStart(Animator animation) {
+        isEnlarging = true;
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animation) {
+        isEnlarging = false;
+        if (isEnlarged) {
+            setScaleX(1f);
+            setScaleY(1f);
+        } else {
+            setScaleX(3f);
+            setScaleY(3f);
+        }
+
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animation) {
 
     }
 
